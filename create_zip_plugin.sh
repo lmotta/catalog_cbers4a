f2193ac756df0d5921dc81dc0f1a7ff3bbd9128b
Luiz Motta <motta.luiz@gmail.com>
2020-11-24 17:03:59 -0300

    Added LICENSE

--- create_zip_plugin.sh
+++ create_zip_plugin.sh
@@ -5,7 +5,9 @@ if [ -f "./$plugin_dir.zip" ]; then
 fi
 mkdir "./$plugin_dir"
 cp *.py "./$plugin_dir"
-for item in metadata.txt LICENSE form.ui planet.svg pl_scenes.qml; do cp "./$item" "./$plugin_dir"; done
+cp *.svg "./$plugin_dir"
+cp *.qml "./$plugin_dir"
+for item in metadata.txt LICENSE form.ui; do cp "./$item" "./$plugin_dir"; done
 zip -r $plugin_dir $plugin_dir
 rm -r $plugin_dir
 #
